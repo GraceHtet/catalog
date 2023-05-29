90da8ba2f5750831c1cc495bd431c0797ed8b082
Thiri <thirihtet246@gmail.com>
2023-05-29 21:05:56 +0630

    Prepare Tdd for can_be_archived? method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,4 +7,10 @@ describe 'Book' do
             expect(book).to be_instance_of Book
         end
     end
+
+    context 'can_be_archived?' do
+        it 'should return true if parent method return true or if cover_state equals to "bad"' do
+            expect(book.can_be_archived?).to be_truthy
+        end 
+    end
 end

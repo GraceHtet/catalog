5c7930127b9eea02d288153f8976751710e4f91e
Nahom <zerihunnahom@gmail.com>
2023-05-29 23:32:47 +0300

    Add test case for add_item method

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,10 +3,17 @@ require_relative '../lib/author'
 describe Author do
     before(:each) do
         @author = Author.new('James','Zerihun')
-      end
+    end
     describe '#initialize' do
         it 'should create an instance with attributes, ' do
           expect(@author).to be_instance_of Author
         end
-      end
+    end
+    describe '#add_item' do
+        it 'should change items length to 1 when we add one item' do
+            item = Item.new('2002-12-04', false)
+            @author.add_item(item)
+            expect(@author.items.length).to be 1
+        end
+    end
 end

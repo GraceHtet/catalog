ddcd35c918823b260272d41bbe909807a9a3cede
Nahom <zerihunnahom@gmail.com>
2023-05-29 18:07:22 +0300

    Add a second test to test can_be_archived method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,19 @@
+require_relative './spec_helper'
 describe Game do
     before(:each) do
-        @game = Game.new('player1', Date.new(2023, 5, 22))
+        @game = Game.new('player1', '2020-12-04')
     end
+
     describe '#initialize' do
         it 'should create an instance with attributes, multiplayer and last_played_at,' do
             expect(@game).to be_instance_of Game
+        end
+    end
+
+    describe '#can_be_archived' do
+        it 'should return true if parent method returns true and last_played_at is older than 2 years' do
+            @game.publish_date = '2012-01-03'
+            expect(@game.can_be_archived?).to be_truthy
         end
     end
 end

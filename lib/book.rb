4d1fbda1d21fa6bb0435e0df6df4ad0c53c05e81
Thiri <thirihtet246@gmail.com>
2023-05-29 21:22:52 +0630

    Implemented can_be_archived? method

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,13 @@ require_relative 'item'
 
 class Book < Item
     
-    def initialize(publisher, cover_state)
+    def initialize(publisher, cover_state, publish_date, id: Random.rand(1..1000))
+        super(publish_date,archived, id: id)
         @publisher = publisher
         @cover_state = cover_state
+    end
+
+    def can_be_archived?
+        super || @cover_state.downcase == 'bad'
     end
 end

850b4c6b5120cd85c5ddcdc25191d239ee53d4cc
Nahom <zerihunnahom@gmail.com>
2023-05-29 18:08:30 +0300

    Update game class to add can_be_archived method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,13 @@
-class Game
+require_relative './item'
+
+class Game < Item
     def initialize(multiplayer,last_played_at)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
+    end
+    def can_be_archived?
+        current_year = Date.today.year
+        last_played_parsed= Date.parse(@last_played_at).year
+        super && current_year - last_played_parsed > 2
     end
 end

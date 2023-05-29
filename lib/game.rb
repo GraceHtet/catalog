3753d588625de5cd840942388d7fe35bcb90530b
Nahom <zerihunnahom@gmail.com>
2023-05-29 18:39:59 +0300

    Update game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,16 @@
 require_relative './item'
 
 class Game < Item
-    def initialize(multiplayer,last_played_at)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
-    def can_be_archived?
-        current_year = Date.today.year
-        last_played_parsed= Date.parse(@last_played_at).year
-        super && current_year - last_played_parsed > 2
-    end
+    attr_accessor :last_played_at
+  def initialize(publish_date,archived,multiplayer, last_played_at)
+    super(publish_date,archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    current_year = Date.today.year
+    last_played_parsed = Date.parse(@last_played_at).year
+    super && current_year - last_played_parsed > 2
+  end
 end

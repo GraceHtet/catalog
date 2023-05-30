926128e62b5bd282e48bf5a659f43f78aff8ebe7
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-05-30 08:00:02 +0100

    Fix linter error

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -1,12 +1,12 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    def initialize(on_spotify, publish_date, archived, id: Random.rand(1..1000))
-        super(publish_date, archived, id: id)   
-        @on_spotify = on_spotify 
-    end
+  def initialize(on_spotify, publish_date, archived, id: Random.rand(1..1000))
+    super(publish_date, archived, id: id)
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived?
-        super && @on_spotify
-    end
+  def can_be_archived?
+    super && @on_spotify
+  end
 end

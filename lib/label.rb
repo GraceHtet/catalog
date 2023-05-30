09e07a33388869e4664a01bb6f7249d43227ab9d
Thiri <thirihtet246@gmail.com>
2023-05-30 08:31:31 +0630

    Fix linters

--- lib/label.rb
+++ lib/label.rb
@@ -1,14 +1,15 @@
 class Label
-    attr_reader :items
-    def initialize(title, color, id: Random.rand(1..1000))
-        @id = id
-        @title = title
-        @color = color
-        @items = []
-    end
+  attr_reader :items
 
-    def add_item(item)
-        @items << item unless self.items.include?(item)
-        item.label = self 
-    end
+  def initialize(title, color, id: Random.rand(1..1000))
+    @id = id
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless items.include?(item)
+    item.label = self
+  end
 end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -2,31 +2,31 @@ require_relative '../lib/label'
 require_relative '../lib/item'
 
 describe 'Label' do
-    before(:each) do
-        @label_one = Label.new('Gone with the wind', 'yellow')
-        @item_one = Item.new('2020-12-10', false)
-        
-        @label_one.add_item(@item_one)
-        @label_one.add_item(@item_one)
+  before(:each) do
+    @label_one = Label.new('Gone with the wind', 'yellow')
+    @item_one = Item.new('2020-12-10', false)
+
+    @label_one.add_item(@item_one)
+    @label_one.add_item(@item_one)
+  end
+
+  context 'initializing' do
+    it 'should be instance of Label' do
+      expect(@label_one).to be_instance_of Label
+    end
+  end
+
+  context 'with Label methods' do
+    it 'should add the input item to the collection of items' do
+      expect(@label_one.items).to include @item_one
     end
 
-    context 'initializing' do
-        it 'should be instance of Label' do
-            expect(@label_one).to be_instance_of Label
-        end
+    it 'should have the same label in item class' do
+      expect(@item_one.label).to be @label_one
     end
 
-    context 'with Label methods' do
-        it 'should add the input item to the collection of items' do
-            expect(@label_one.items).to include @item_one
-        end
-
-        it 'should have the same label in item class' do
-           expect(@item_one.label).to be @label_one
-        end 
-
-        it 'length should be one if the same item was added' do
-            expect(@label_one.items.length).to eq 1
-         end 
+    it 'length should be one if the same item was added' do
+      expect(@label_one.items.length).to eq 1
     end
+  end
 end

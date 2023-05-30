5d98de3512b127fd82a492c6cf80509e69c18afa
Nahom <zerihunnahom@gmail.com>
2023-05-30 22:41:58 +0300

    Update storage

--- module/storage.rb
+++ module/storage.rb
@@ -1,7 +1,35 @@
 require 'json'
 require_relative '../lib/book'
+require_relative '../lib/label'
 
 module Storage
+  def save_books(books)
+    save_data('./data/book.json', array_to_hash(books))
+  end
+
+  def save_extra_details(label, _genre, author)
+    save_data('./data/author.json', array_to_hash(author))
+    save_data('./data/label.json', array_to_hash(label))
+  end
+
+  def fetch_books
+    books = fetch_data('./data/book.json')
+    book_arr = []
+    books&.each do |book|
+      book_sample = Book.new(book['publisher'], book['cover_state'], book['publish_date'])
+      extra_to_array(book_sample, book)
+      book_arr << book_sample
+    end
+    book_arr
+  end
+
+  def fetch_labels
+    labels = fetch_data('./data/label.json')
+    labels&.map do |label|
+      Label.new(label['title'], label['color'])
+    end
+  end
+
   private
 
   def save_data(file_path, data)
@@ -16,7 +44,14 @@ module Storage
     JSON.parse(json_data) unless json_data.empty?
   end
 
-  def array_to_hash(array)
-    array.map(&:to_hash)
+  def array_to_hash(arr)
+    arr.map(&:to_hash)
+  end
+
+  def extra_to_array(klass, data)
+    label = Label.new(data['label']['title'], data['label']['color'])
+    author = Author.new(data['author']['first_name'], data['author']['last_name'])
+    klass.send(:label=, label)
+    klass.send(:author=, author)
   end
 end

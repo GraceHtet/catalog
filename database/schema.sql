d71c952b38da65d8ba74d14f78bdb9560bbb77cd
Nahom <zerihunnahom@gmail.com>
2023-05-31 14:08:44 +0300

    Create queries.sql file to store all queries

--- database/schema.sql
+++ database/schema.sql
@@ -38,25 +38,3 @@ CREATE TABLE games (
     label_id INT REFERENCES labels(id),
     PRIMARY KEY (id, genre_id, author_id, label_id)
 );
-
--- INSERT INTO authors(first_name, last_name)
--- VALUES ('mil','grace'),
--- ('aung','swe');
-
--- INSERT INTO labels(title, color)
--- VALUES ('old','grey'),
--- ('new','gold');
-
--- INSERT INTO genres(name)
--- VALUES ('horror'),
--- ('fantasy');
-
--- INSERT INTO items(publish_date, archived)
--- VALUES ('2021-03-03', true),
--- ('2023-08-25', false);
-
--- INSERT INTO books(pulblisher, cover_state, item_id, genre_id, author_id, label_id)
--- VALUES ('moh moh','good',1,2,1,2),
--- ('mee mee','good',1,2,1,2);
-
--- SELECT B.pulblisher, B.cover_state, I.publish_date,A.first_name, G.name, L.title FROM books B JOIN items I ON B.item_id = I.id JOIN genres G ON b.genre_id = G.id JOIN authors A ON b.author_id = A.id JOIN labels L ON B.label_id = L.id;
